598069fcbbc1b7056805ec3bc220c5dbfb7755ea
Rayner Vintervoll <rayner.vintervoll@accenture.com>
2015-02-26 13:08:54 +0000

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+   $scope.click = function(arg) {
+    alert('Clicked ' + arg);
+  }
+  $scope.html = '<a ng-click="click(1)" href="#">Click me</a>';
+  
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
